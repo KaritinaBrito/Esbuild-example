6f65a9f409b81cde3c21daeeb4a568a0ded68265
Karen Brito <karitinabrito@gmail.com>
2023-04-14 08:44:36 -0600

    deploy

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+//import { ReactDOM } from "react";
+import App from './components/App';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <App message="Platzi ESBuild example"/>,
+    document.getElementById("App")
+);
